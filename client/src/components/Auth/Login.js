2a0e4e6ee03bb79d674d7b0aeb4ccb8e3a691c79
Zachary McManus <zacharymcmanus@gmail.com>
2019-09-05 00:17:48 -0700

    first commit

---
+++ client/src/components/Auth/Login.js
@@ -0,0 +1,64 @@
+import React, { useContext } from 'react';
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+
+import Context from '../../context';
+import { ME_QUERY } from '../../graphql/queries';
+
+const Login = ({ classes }) => {
+    const { dispatch } = useContext(Context);
+
+    const onFailure = err => {
+        console.error('Error loggin in', err);
+        dispatch({ type: 'IS_LOGGED_IN', payload: false })
+    };
+
+    const onSuccess = async googleUser => {
+        try {
+            const idToken = googleUser.getAuthResponse().id_token;
+            const client = new GraphQLClient('http://localhost:4000/graphql', {
+                headers: { authorization: idToken }
+            });
+            const { me } = await client.request(ME_QUERY);
+            dispatch({ type: 'LOGIN_USER', payload: me });
+            dispatch({ type: 'IS_LOGGED_IN', payload: googleUser.isSignedIn() });
+        } catch (err) {
+            onFailure(err);
+        }
+    };
+
+    return (
+        <div className={classes.root}>
+            <Typography 
+                component="h1"
+                variant="h3"
+                gutterBottom
+                noWrap
+                style={{ color: "rgb(66, 133, 244)" }}
+            >
+                GeoPins
+            </Typography>
+            <GoogleLogin
+                clientId="157969270500-1678jtrc39vkjgmtajdsa2arcurr65dk.apps.googleusercontent.com"
+                onSuccess={onSuccess}
+                isSignedIn={true}
+                onFailure={onFailure}
+                theme="dark"
+            />
+        </div>
+    );
+};
+
+const styles = {
+    root: {
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+};
+
+export default withStyles(styles)(Login);
