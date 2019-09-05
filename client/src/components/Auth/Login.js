11f45d5190574d2d696476aba5b7d9944850e863
Zachary McManus <zacharymcmanus@gmail.com>
2019-09-05 16:26:43 -0700

    updated build settings

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -2,28 +2,32 @@ import React, { useContext } from 'react';
 import { GraphQLClient } from 'graphql-request';
 import { GoogleLogin } from 'react-google-login';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from "@material-ui/core/Typography";
+import Typography from '@material-ui/core/Typography';
 
 import Context from '../../context';
 import { ME_QUERY } from '../../graphql/queries';
+import { BASE_URL } from "../../client";
 
 const Login = ({ classes }) => {
     const { dispatch } = useContext(Context);
 
     const onFailure = err => {
         console.error('Error loggin in', err);
-        dispatch({ type: 'IS_LOGGED_IN', payload: false })
+        dispatch({ type: 'IS_LOGGED_IN', payload: false });
     };
 
     const onSuccess = async googleUser => {
         try {
             const idToken = googleUser.getAuthResponse().id_token;
-            const client = new GraphQLClient('http://localhost:4000/graphql', {
+            const client = new GraphQLClient(BASE_URL, {
                 headers: { authorization: idToken }
             });
             const { me } = await client.request(ME_QUERY);
             dispatch({ type: 'LOGIN_USER', payload: me });
-            dispatch({ type: 'IS_LOGGED_IN', payload: googleUser.isSignedIn() });
+            dispatch({
+                type: 'IS_LOGGED_IN',
+                payload: googleUser.isSignedIn()
+            });
         } catch (err) {
             onFailure(err);
         }
@@ -31,21 +35,21 @@ const Login = ({ classes }) => {
 
     return (
         <div className={classes.root}>
-            <Typography 
-                component="h1"
-                variant="h3"
+            <Typography
+                component='h1'
+                variant='h3'
                 gutterBottom
                 noWrap
-                style={{ color: "rgb(66, 133, 244)" }}
+                style={{ color: 'rgb(66, 133, 244)' }}
             >
-                GeoPins
+                WanderPins
             </Typography>
             <GoogleLogin
-                clientId="157969270500-1678jtrc39vkjgmtajdsa2arcurr65dk.apps.googleusercontent.com"
+                clientId='157969270500-1678jtrc39vkjgmtajdsa2arcurr65dk.apps.googleusercontent.com'
                 onSuccess={onSuccess}
                 isSignedIn={true}
                 onFailure={onFailure}
-                theme="dark"
+                theme='dark'
             />
         </div>
     );
